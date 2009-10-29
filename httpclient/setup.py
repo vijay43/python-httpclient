86a000d89c10286e48951487fd1c4cb0b0a5a4ea
Bruno.Harbulot <Bruno.Harbulot@manchester.ac.uk>
2009-10-29 15:37:24 +0000

    Initial commit.

---
+++ httpclient/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name         = "Python-HTTPclient",
+      version      = "0.3",
+      py_modules   = ['httpclient'],
+      author       = "Bruno Harbulot",
+      author_email = "Bruno.Harbulot@manchester.ac.uk",
+      url          = "http://code.google.com/p/python-httpclient/"
+)
